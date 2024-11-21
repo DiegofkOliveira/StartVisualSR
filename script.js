4dbb0f79e8439d65ec674a7b89fee63dcc8ce41e
Diego Oliveira <diegofkoliveira@gmail.com>
2024-11-21 00:10:16 -0300

    Eight commit

--- script.js
+++ script.js
@@ -39,14 +39,13 @@ infoButtons.forEach(button => {
 
 document.addEventListener("DOMContentLoaded", () => {
     const counters = document.querySelectorAll(".stats-number");
-    
-    // Função para iniciar a contagem
+
     const startCount = (counter) => {
         const target = +counter.getAttribute("data-target");
         let count = 0;
 
         const updateCount = () => {
-            count += Math.ceil(target / 100); // Velocidade da contagem
+            count += Math.ceil(target / 100);
             if (count < target) {
                 counter.innerText = `+${count}`;
                 setTimeout(updateCount, 20);
@@ -58,19 +57,54 @@ document.addEventListener("DOMContentLoaded", () => {
         updateCount();
     };
 
-    // Observador para verificar quando a div está visível
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                counters.forEach(counter => {
-                    counter.innerText = "0"; // Zera o valor antes de contar
-                    startCount(counter); // Inicia a contagem
-                });
+                // Defina o atraso desejado em milissegundos (ex: 1 segundo = 1000ms)
+                setTimeout(() => {
+                    counters.forEach(counter => {
+                        counter.innerText = "0";  // Reseta o contador para 0
+                        startCount(counter);      // Inicia a contagem
+                    });
+                }, 2500); // 1000ms = 1 segundo de atraso
             }
         });
     });
 
-    // Observa a div .stats-container
     const statsContainer = document.querySelector(".stats-container");
     observer.observe(statsContainer);
 });
+
+
+const elements = [
+    { selector: '.imagem-container', delay: 0.6 },
+    { selector: '.video-wrapper video', delay: 0.9 },
+    { selector: '.logo-item', delay: 0.9 },
+    { selector: '.stats-item', delay: 0.9 },
+    { selector: '.contact-wrapper', delay: 0.9 },
+    { selector: '.contact-link', delay: 0.9 },
+    { selector: '.footer-item', delay: 0.9 },
+    { selector: '.footer-location', delay: 1 }
+];
+
+
+const applyObserver = (elements) => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    });
+
+    elements.forEach(({ selector, delay }) => {
+        const items = document.querySelectorAll(selector);
+        items.forEach((item, index) => {
+            item.style.transitionDelay = `${index * delay}s`;
+            observer.observe(item);
+        });
+    });
+};
+
+
+applyObserver(elements);
